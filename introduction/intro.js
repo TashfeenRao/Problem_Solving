1bd047d7a590f58a11484285bb0bbb610289661a
tashfeen rao <tashfeen@ricult.com>
2022-01-09 22:13:32 +0500

    sliding window algo added

--- introduction/intro.js
+++ introduction/intro.js
@@ -2,16 +2,31 @@
 // Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
 // Output: [2.2, 2.8, 2.4, 3.6, 2.8]
 
-const arr = [1, 3, 2, 6, -1, 4, 1, 8, 2];
+const arr1 = [1, 3, 2, 6, -1, 4, 1, 8, 2];
 const k = 5;
 const result = [];
 
-for (let i = 0; i < arr.length; i++) {
+// brute force solution
+for (let i = 0; i < arr1.length; i++) {
   let sum = 0;
-  for (let j = i; j < k; j++) {
-    sum += arr[j];
+  for (let j = i; j < k + i; j++) {
+    sum += arr1[j];
   }
-  arr.push(sum / k);
+  if (sum) result.push(sum / k);
+}
+
+// sliding window solution
+//
+let windowStart = 0,
+  sum = 0;
+for (let windowEnd = 0; windowEnd < arr1.length; windowEnd++) {
+  sum += arr1[windowEnd];
+
+  if (windowEnd >= k - 1) {
+    result.push(sum / k);
+    sum -= arr1[windowStart];
+    windowStart += 1;
+  }
 }
 
 console.log(result);
