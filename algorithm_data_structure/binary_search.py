4f9ef722ebb11932cef5bdebea8bc5360efef2f5
Tashfeen Rao <raotashfeen25@gmail.com>
2022-08-28 01:35:01 +0500

    implemented the binary search

--- algorithm_data_structure/binary_search.py
+++ algorithm_data_structure/binary_search.py
@@ -5,7 +5,7 @@ def binary_search(find_me_in_list: int, the_list: list):
     end = list_length - 1
 
     while start <= end:
-        mid = start + end
+        mid = int((start + end) / 2)
         if the_list[mid] == find_me_in_list:
             return print("Yes! we find it in the sorted list at index", mid)
         elif the_list[mid] < find_me_in_list:
@@ -15,4 +15,4 @@ def binary_search(find_me_in_list: int, the_list: list):
     return print("Sorry! we could not find this number in the list")
 
 
-binary_search(61, [2, 3, 5, 61, 2])
+binary_search(1, [2, 3, 5, 61, 2])
