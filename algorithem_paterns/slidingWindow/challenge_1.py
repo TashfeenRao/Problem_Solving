d8008d1c7ad3d7899a656b94532701f7a005698a
Tashfeen Rao <tashfeen@ricult.com>
2023-03-12 02:30:43 +0500

    added coment

--- algorithem_paterns/slidingWindow/challenge_1.py
+++ algorithem_paterns/slidingWindow/challenge_1.py
@@ -1,7 +1,6 @@
 # Given a string and a pattern, find out if the string contains any permutation of the pattern.
-#
-# Permutation is defined as the re-arranging of the characters of the string. For example, “abc” has the following six permutations:
-#
+# Permutation is defined as the re-arranging of the characters of the string. For example, 
+#“abc” has the following six permutations:
 # abc
 # acb
 # bac
@@ -9,12 +8,8 @@
 # cab
 # cba
 # If a string has ‘n’ distinct characters it will have
-# �
-# !
 # n! permutations.
-#
 # Example 1:
-#
 # Input: String="oidbcaf", Pattern="abc"
 # Output: true
 # Explanation: The string contains "bca" which is a permutation of the given pattern.
@@ -34,25 +29,34 @@ def challenge_1(string:str, pattern:str):
     window_start, matched = 0, 0
     char_frequency = {}
     
+    # making the count of each character in pattern
     for char in pattern:
         if char not in char_frequency:
             char_frequency[char] = 0
         char_frequency[char] += 1
             
     for window_end in range(len(string)):
+        # checking if string has same number of characters as we recorded for pattern
+
         right_char = string[window_end]
         
+        # when we char from string in our frequency we decrease it by 1
+        # if we have matching char and we reached 0 count in frequency we found a match we increase the match
         if right_char in char_frequency:
             char_frequency[right_char] -= 1
             if char_frequency[right_char] == 0:
                 matched += 1
+        # length of the char frequency obj and matched variable are equal 
+        # than we conclude all the char in frequency with their number of apperences present in string
         if matched == len(char_frequency):
             return True
         
+        # when we reach window length at the pattern length we move the starting point of the window forward
         if window_end >= len(pattern)-1:
             left_char = string[window_start]
             window_start += 1
-            
+            # we reached the length of the pattern and we did not find exact match then 
+            # we will reset the frequency char and decrease matched by 1
             if left_char in char_frequency:
                 if char_frequency[left_char] == 0:
                     matched -= 1
